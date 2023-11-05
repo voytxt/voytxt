9f744d24de70b3702e36a473248309ca871780f3
Vojta <65811166+vojta-dev@users.noreply.github.com>
2023-11-05 13:39:15 +0100

    aim trainer

---
+++ src/routes/aim/+page.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+  let score = 0;
+  let highlightedTiles: number[] = [];
+
+  function highlightRandomTile() {
+    let randomTile: number;
+
+    do {
+      randomTile = Math.floor(Math.random() * 16);
+    } while (highlightedTiles.includes(randomTile));
+
+    highlightedTiles.push(randomTile);
+  }
+
+  function handleClick(tile: number) {
+    if (highlightedTiles.includes(tile)) {
+      score++;
+      highlightRandomTile();
+      highlightedTiles = highlightedTiles.filter((t) => t !== tile);
+    } else {
+      score -= 10;
+    }
+  }
+
+  highlightRandomTile();
+  highlightRandomTile();
+  highlightRandomTile();
+</script>
+
+<div>{score}</div>
+<main>
+  {#each { length: 16 } as _, tile}
+    <button
+      class:highlighted={highlightedTiles.includes(tile)}
+      on:click={() => handleClick(tile)}
+    />
+  {/each}
+</main>
+
+<style>
+  div {
+    margin: 1rem;
+    font-size: 3rem;
+    text-align: center;
+  }
+
+  main {
+    display: grid;
+    grid-template: repeat(4, min(15vh, 15vw)) / repeat(4, min(15vh, 15vw));
+    background-color: black;
+    margin: 0 auto;
+    width: max-content;
+  }
+
+  button {
+    border: 2px solid white;
+    cursor: pointer;
+    transition: background-color 0.15s ease-in;
+  }
+  button.highlighted {
+    background-color: #a5f3fc;
+  }
+</style>
