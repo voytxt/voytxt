f5e0863af7b3175644d0a0eafbc7f59499b4fa82
Vojta <65811166+voytxt@users.noreply.github.com>
2023-11-18 09:34:31 +0100

    other external projects

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,16 +14,21 @@
   </button>
 
   <main class="teal">
+    <Project time="2023-07" name="pickle hunt" link="https://github.com/voytxt/pickle-hunt" />
     <Project time="2023-05" name="geocaching stats" link="/gcstats" />
     <Project time="2023-02" name="geocache lookup" link="/gc" />
+    <Project time="2023-02" name="flashlight" link="https://github.com/voytxt/flashlight" />
+    <Project time="2022-12" name="popeláři" link="https://github.com/voytxt/popelari" />
     <Project time="2022-09" name="translator" link="/translator" />
     <Project time="2022-09" name="TLGE2 explorer" link="https://tlge2.vercel.app/" />
+    <Project time="2022-05" name="bio changer" link="https://github.com/voytxt/about-me-changer" />
     <Project time="2022-05" name="czechia" link="/czechia" />
     <Project time="2022-05" name="old portfolio 2" link="https://vojta.vercel.app/" />
     <Project time="2022-03" name="wocabee bot" link="https://wocabot.vercel.app/" />
     <Project time="2022-03" name="random image" link="/image" />
     <Project time="2022-03" name="16 assholes" link="/16" />
     <Project time="2022-02" name="password generator" link="/password" />
+    <Project time="2022-02" name="robtop bot" link="https://github.com/voytxt/robtop-bot" />
     <Project time="2021-10" name="snake" link="/snake" />
     <Project time="2021-08" name="aim trainer" link="/aim" />
     <Project time="2021-08" name="old portfolio" link="/repos" />
