581c50daf20ce4c2b9004261caa606eb6f2208eb
Vojtěch Křižan <voytxt@voytxt.com>
2024-12-22 14:54:00 +0100

    add simple error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { page } from '$app/state';
+</script>
+
+<h1>{page.status} {page.error?.message}</h1>
+<img src="https://media1.tenor.com/m/TeW54nXF2QAAAAAC/cat.gif" alt="funny cat" />
+
+<style>
+  h1 {
+    text-align: center;
+    margin-top: 2rem;
+  }
+
+  img {
+    display: block;
+    margin: 1rem auto;
+  }
+</style>
