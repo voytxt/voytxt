17a3ea48d0e44d9a587b2b446320fbb7420e00ef
Vojtěch Křižan <voytxt@voytxt.com>
2024-12-25 21:15:46 +0100

    update /desmos

--- src/routes/desmos/+page.svelte
+++ src/routes/desmos/+page.svelte
@@ -1,38 +1,91 @@
 <script lang="ts">
   import { onMount } from 'svelte';
-  import { SIZE } from '.';
-  import { draw } from './draw';
-  import { parse } from './parse';
+
+  const SIZE = 800;
+  const PRECISION = 0.1;
+  const COLORS = ['red', 'blue', 'green', 'purple', 'black'];
 
   let canvas = $state<HTMLCanvasElement>();
   let input = $state<string>('');
-  let c = $state<CanvasRenderingContext2D>();
+  let ctx = $state<CanvasRenderingContext2D>();
 
   onMount(() => {
-    c = canvas!.getContext('2d')!;
-    handleInput();
+    ctx = canvas!.getContext('2d')!;
+    draw([]);
   });
 
   function handleInput() {
-    if (c === undefined) return;
+    if (ctx === undefined) return;
+    draw(parse(input));
+  }
 
-    const points = parse(input);
-    draw(c, points);
+  // warning: very cursed
+  function parse(equation: string): [number, number][] {
+    const points: [number, number][] = [];
+
+    const globalMath = `const { ${Object.getOwnPropertyNames(Math).join(', ')} } = Math; const [pi, e, rnd] = [PI, E, random];`;
+
+    try {
+      for (let x = -(SIZE / 2); x < SIZE / 2; x += PRECISION) {
+        const y = eval(`${globalMath} ${equation}`) as number;
+        points.push([x, y]);
+      }
+
+      return points;
+    } catch {
+      return [];
+    }
+  }
+
+  function draw(points: [number, number][]) {
+    if (ctx === undefined) throw 'undefined canvas context';
+
+    ctx.clearRect(0, 0, SIZE, SIZE);
+
+    // x and y axis
+    ctx.fillStyle = 'black';
+    ctx.fillRect(SIZE / 2, 0, 1, SIZE);
+    ctx.fillRect(0, SIZE / 2, SIZE, 1);
+
+    // the function
+    ctx.fillStyle = COLORS[Math.floor(Math.random() * COLORS.length)];
+    for (const [x, y] of points) {
+      ctx.fillRect(SIZE / 2 + x - 1, SIZE / 2 - y - 1, 2, 2);
+    }
   }
 </script>
 
-<h1>Chess Battle Desmos</h1>
-<div>
+<h1>Chess Battle Desmos ({SIZE}x{SIZE} grid)</h1>
+<main>
   <canvas bind:this={canvas} width={SIZE} height={SIZE}></canvas>
   <span>y=<input bind:value={input} oninput={() => handleInput()} type="text" /></span>
+</main>
+<div>
+  cool equations:
+  <pre>rnd()*{SIZE}-{SIZE / 2}</pre>
+  <pre>sin(x)*x</pre>
+  <pre>tan(x)*x</pre>
+  <pre>{SIZE / 4}/tan(x)</pre>
+  <pre>tan(x*pi)*x</pre>
+  <pre>sin({SIZE}/x)*tan(x*pi)*x</pre>
+  <small>do you have more? message me on discord (@voytxt) and i may add them lol</small>
 </div>
+
+<svelte:head>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+  <link
+    href="https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap"
+    rel="stylesheet"
+  />
+</svelte:head>
 
 <style>
   h1 {
     text-align: center;
   }
 
-  div {
+  main {
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -40,12 +93,35 @@
 
     canvas {
       border: 1px solid black;
+      max-width: 90vw;
     }
 
     span,
     input {
       font-size: 3rem;
       font-family: 'JetBrains Mono';
+
+      @media (width < 800px) {
+        font-size: 1.5rem;
+      }
+    }
+  }
+
+  div {
+    text-align: center;
+    font-size: 2rem;
+    margin: 1rem;
+
+    @media (width < 800px) {
+      font-size: 1rem;
+    }
+
+    * {
+      margin: 0;
+    }
+
+    small {
+      font-size: 0.75em;
     }
   }
 </style>
--- src/routes/desmos/index.ts
+++
@@ -1,2 +0,0 @@
-export const SIZE = 800;
-export const PRECISION = 0.1;
