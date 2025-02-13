7b54507ecef5009362141c5e3b33079ed46abe64
Vojtěch Křižan <voytxt@voytxt.com>
2025-02-13 19:40:42 +0100

    get rid of global styles warnings

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,7 @@
 <script lang="ts">
+  import './styles.css';
+
   const { children } = $props();
 </script>
 
 {@render children()}
-
-<style>
-  :global(body) {
-    font-family:
-      system-ui,
-      -apple-system,
-      sans-serif;
-    margin: 0;
-  }
-
-  :global(body:has(.homepage)) {
-    background-color: #6366f1;
-  }
-</style>
