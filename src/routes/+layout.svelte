64a1ad8177df4740df747dbdf650b15ffa64b2fb
Vojtěch Křižan <voytxt@voytxt.com>
2025-05-10 15:11:12 +0200

    try out vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
+  import { dev } from '$app/environment';
+  import { injectAnalytics } from '@vercel/analytics/sveltekit';
   import './styles.css';
+
+  injectAnalytics({ mode: dev ? 'development' : 'production' });
 
   const { children } = $props();
 </script>
