f20bd2c217ee4271bb7472b55f506c40a28e6e96
Vojtěch Křižan <voytxt@voytxt.com>
2025-07-05 13:18:27 +0200

    specify browser support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
+// https://vite.dev/guide/rolldown
+// https://github.com/vitejs/vite/discussions/13835
+// https://github.com/sveltejs/vite-plugin-svelte/issues/1143
+
 import { sveltekit } from '@sveltejs/kit/vite';
+import browserslist from 'browserslist';
+import browserslistToEsbuild from 'browserslist-to-esbuild';
+import { browserslistToTargets } from 'lightningcss';
 import { defineConfig } from 'vite';
+
+const bl = browserslist('since 2025');
 
 export default defineConfig({
   plugins: [sveltekit()],
   experimental: { enableNativePlugin: true },
+
+  build: { target: browserslistToEsbuild(bl) },
+  css: {
+    transformer: 'lightningcss',
+    lightningcss: { targets: browserslistToTargets(bl) },
+  },
 });
