4e403a8e6c8295cb414254c8b24aef923c6e03a1
Vojtěch Křižan <voytxt@voytxt.com>
2026-03-18 17:31:11 +0100

    don't use browserslist anymore

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,7 @@
-// https://vite.dev/guide/rolldown
-// https://github.com/vitejs/vite/discussions/13835
-// https://github.com/sveltejs/vite-plugin-svelte/issues/1143
-
 import { sveltekit } from '@sveltejs/kit/vite';
-import browserslist from 'browserslist';
-import browserslistToEsbuild from 'browserslist-to-esbuild';
-import { browserslistToTargets } from 'lightningcss';
 import { defineConfig } from 'vite';
-
-const bl = browserslist('since 2025');
 
 export default defineConfig({
   plugins: [sveltekit()],
-
-  build: { target: browserslistToEsbuild(bl) },
-  css: {
-    transformer: 'lightningcss',
-    lightningcss: { targets: browserslistToTargets(bl) },
-  },
+  build: { target: ['chrome140', 'edge140', 'firefox140', 'safari26'] },
 });
